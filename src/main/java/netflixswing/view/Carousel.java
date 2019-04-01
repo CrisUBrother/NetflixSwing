3a232fc65a6ec351ba7b395e8b344f927e94d865
naiipe <rfarias@mcfadyen.com>
2019-04-01 00:19:44 -0300

    setBackgroundImage finished

--- src/main/java/netflixswing/view/Carousel.java
+++ src/main/java/netflixswing/view/Carousel.java
@@ -3,16 +3,27 @@ package netflixswing.view;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class Carousel extends javax.swing.JPanel {
 
-    public static void main(String[] args) throws IOException {
+    public void setBackgroundImage() throws IOException {
 
-        BufferedImage image = ImageIO.read(new File("/netflixswing/view/gradientblack.jpg"));
-        JLabel picLabel = new JLabel(new ImageIcon(image));
+        //PUT YOUR DIRECTORY'S FIRST STEPS
+        BufferedImage img = ImageIO.read(new File("PUT_YOUR_DIRECTORY_PATH/Documents/NetflixSwing/src/main/java/netflixswing/view/gradientblack.jpg"));
+        ImageIcon icon = new ImageIcon(img);
+        JFrame frame = new JFrame();
+        frame.setLayout(new FlowLayout());
+        frame.setSize(1200, 1300);
+        frame.setExtendedState( frame.getExtendedState()|JFrame.MAXIMIZED_BOTH );
+        JLabel lbl = new JLabel();
+        lbl.setIcon(icon);
+        frame.add(lbl);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
 }
